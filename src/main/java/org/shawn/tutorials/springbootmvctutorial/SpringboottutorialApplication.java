fa55cb2d62f4c029451788fddb804eae47d30f65
Shawn <1924371723@qq.com>
2015-11-21 11:22:26 +0800

    典型配置

--- src/main/java/org/shawn/tutorials/springbootmvctutorial/SpringboottutorialApplication.java
+++ src/main/java/org/shawn/tutorials/springbootmvctutorial/SpringboottutorialApplication.java
@@ -1,23 +1,13 @@
 package org.shawn.tutorials.springbootmvctutorial;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-public class SpringboottutorialApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class SpringboottutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringboottutorialApplication.class, args);
     }
 
-    @Override
-    protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-        return application.sources(SpringboottutorialApplication.class);
-    }
 }
