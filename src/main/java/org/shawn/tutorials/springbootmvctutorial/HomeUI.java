307a9d52b65b6dd0db87e798358734983b251fdd
Shawn <1924371723@qq.com>
2015-11-22 10:35:11 +0800

    在vaadin中检查权限

--- src/main/java/org/shawn/tutorials/springbootmvctutorial/HomeUI.java
+++ src/main/java/org/shawn/tutorials/springbootmvctutorial/HomeUI.java
@@ -1,10 +1,18 @@
 package org.shawn.tutorials.springbootmvctutorial;
 
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+
+import java.security.Principal;
+
 
 /**
  * Created by Shawn on 15/11/22.
@@ -13,9 +21,20 @@ import com.vaadin.ui.UI;
 @SpringUI
 public class HomeUI extends UI {
 
-
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("<h1>Vaadin Home</h1>", ContentMode.HTML));
+        Principal user = vaadinRequest.getUserPrincipal();
+        Authentication authentication = (Authentication) user;
+        if (VaadinService.getCurrentRequest().isUserInRole("ADMIN"))
+            setContent(new Label(String.format("<h1>Vaadin Home [%s]</h1>", user.getName()), ContentMode.HTML));
+        else
+            setContent(new Label(String.format("<h1>Unauthorize access</h1>", user.getName()), ContentMode.HTML));
+
+        System.err.println(authentication.getName());
+        System.err.println(authentication.getAuthorities().size());
+        for (GrantedAuthority authority : authentication.getAuthorities()) {
+            System.err.println(authority.getAuthority());
+        }
+
     }
 }
